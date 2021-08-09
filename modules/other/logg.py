9c2b35127faf9fd6000b804023f4b06e1b4a09ca
Alex <alex.ling@bfjfunds.com.au>
2021-08-09 13:54:40 +1000

    update logger

--- modules/other/logg.py
+++ modules/other/logg.py
@@ -1,4 +1,5 @@
 import logging
+import datetime
 import sys
 import os
 sys.path.append(os.path.join(os.getcwd().split('xtraderbacktest')[0],'xtraderbacktest'))
@@ -10,7 +11,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # create file handler
-log_path = sys_conf_loader.get_sys_path() + "/data/logs/" + "test.log"
+log_path = sys_conf_loader.get_sys_path() + "/data/logs/" + datetime.datetime.now().strftime("%Y-%m-%d") + ".log"
 if sys.platform.startswith('linux') == False:
     log_path = log_path.replace('/','\\')
 
@@ -19,4 +20,14 @@ fmt = "%(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d %(messag
 datefmt = "%a %d %b %Y %H:%M:%S"
 formatter = logging.Formatter(fmt, datefmt)
 logging.basicConfig(filename=log_path,level= logging.INFO,datefmt=datefmt,format=fmt)
+logging.basicConfig(filename=log_path,level= logging.DEBUG,datefmt=datefmt,format=fmt)
+logging.basicConfig(filename=log_path,level= logging.WARNING,datefmt=datefmt,format=fmt)
+logging.basicConfig(filename=log_path,level= logging.ERROR,datefmt=datefmt,format=fmt)
+logging.basicConfig(filename=log_path,level= logging.CRITICAL,datefmt=datefmt,format=fmt)
 
+if __name__ == "__main__":
+    logging.info("test info message")
+    logging.debug("test debug message")
+    logging.warning("test warning message")
+    logging.error("test error message")
+    logging.critical("test critical message")
