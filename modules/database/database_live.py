5e7f7eec0ff56a0f99012c8ddf23ea80a3d2ecb0
Alex <alex.ling@bfjfunds.com.au>
2021-09-15 11:24:12 -0400

    update

--- modules/database/database_live.py
+++ modules/database/database_live.py
@@ -3,7 +3,8 @@ import sys
 sys.path.append(os.path.join(os.getcwd().split('xtraderbacktest')[0],'xtraderbacktest'))
 import modules.other.sys_conf_loader as sys_conf_loader
 import modules.database.mango as mango
-
+import modules.other.logg
+import logging 
 
 is_on = sys_conf_loader.get_sys_conf()["live_conf"]["save_order_record"]['is_on']
 db_type = sys_conf_loader.get_sys_conf()["live_conf"]["save_order_record"]['type']
@@ -11,10 +12,13 @@ db_name = sys_conf_loader.get_sys_conf()["live_conf"]["save_order_record"]['db_n
 
 
 def save_json(obj,collection,id= None):
-    if is_on:
-        if db_type == "mangos":
-            mango.save_json(obj,collection,id,db_name)
+    try:
+        if is_on:
+            if db_type == "mangos":
+                mango.save_json(obj,collection,id,db_name)
+    except Exception as e:
+        logging.exception(e)
 
 
 if __name__ == "__main__":
-    save_json({"test":"test content"},"test","test_id")
+    save_json({"test":"test content"},"Positions","test_id")
--- modules/notification/notifaction.py
+++ modules/notification/notifaction.py
@@ -3,6 +3,8 @@ import sys
 sys.path.append(os.path.join(os.getcwd().split('xtraderbacktest')[0],'xtraderbacktest'))
 import modules.other.sys_conf_loader as sys_conf_loader
 import modules.notification.telegram_x as telegram 
+import modules.other.logg
+import logging 
 
 notification_conf = sys_conf_loader.get_sys_conf()["live_conf"]["notification"]
 is_on = notification_conf['is_on']
@@ -11,12 +13,16 @@ notification_type = notification_conf['type']
 
 
 def send_message(message,obj = None):
-    if is_on:
-        if notification_type == "telegram":
-            telegram.send_message(message,obj)
-        if notification_type == "mail":
-            # TBD
-            pass
+    try:
+        if is_on:
+            if notification_type == "telegram":
+                telegram.send_message(message,obj)
+            if notification_type == "mail":
+                # TBD
+                pass
+    except Exception as e:
+        logging.exception(e)
+
 
 
 if __name__ == "__main__":
