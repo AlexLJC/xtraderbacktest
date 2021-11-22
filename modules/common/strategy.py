a184b3f4382c29b3abde955db0cd91ac27cad664
Alex <alex.ling@bfjfunds.com.au>
2021-11-21 21:24:39 -0500

    update

--- modules/common/strategy.py
+++ modules/common/strategy.py
@@ -404,9 +404,7 @@ class Strategy():
         if self._mode == "backtest":
             self.order_manager._filled_all_ing_orders(tick)
 
-        # For temporly use. TBD
-        #if self._mode == "live":
-            #self.order_manager._filled_all_ing_orders(tick)
+
     
     def _round_check_before(self,tick):
         self._set_current_time(tick["date"])
--- modules/notification/notifaction.py
+++ modules/notification/notifaction.py
@@ -4,6 +4,7 @@ sys.path.append(os.path.join(os.getcwd().split('xtraderbacktest')[0],'xtraderbac
 import modules.other.sys_conf_loader as sys_conf_loader
 import modules.notification.telegram_x as telegram 
 import modules.other.logg
+import modules.notification.email_x as email
 import logging 
 
 notification_conf = sys_conf_loader.get_sys_conf()["live_conf"]["notification"]
@@ -12,13 +13,13 @@ notification_type = notification_conf['type']
 
 
 
-def send_message(message,obj = None):
+def send_message(message,obj = None,mail_list = []):
     try:
         if is_on:
             if notification_type == "telegram":
                 telegram.send_message(message,obj)
             if notification_type == "mail":
-                # TBD
+                email.send_mail(message = message,mail_list = mail_list)
                 pass
     except Exception as e:
         logging.exception(e)
